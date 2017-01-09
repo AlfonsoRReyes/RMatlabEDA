2dd62d7120ca0561a755568fd6e00f41cdf9f141
superuser mint18x64 <superuser-mint18x64@reyesaguayo.com>
2017-01-08 21:10:53 -0600

    data definition for X.mvnrnd

---
+++ R/X.mvnrnd.R
@@ -0,0 +1,23 @@
+#' Random number for the normal distribution.
+#'
+#' Random numbers coming from Matlab example.
+#'
+#' @name X.mvnrnd
+#'
+#' @docType data
+#'
+#' @aliases x
+#'
+#' @details
+#' \itemize{
+#' \item \code{V1} - column 1
+#' \item \code{V2} - column 2
+#' }
+#'
+#' @format A data frame 100x2 of random numbers
+#' @keywords datasets
+#'
+#' @examples
+#' X.mvnrnd[5]
+NULL
+
