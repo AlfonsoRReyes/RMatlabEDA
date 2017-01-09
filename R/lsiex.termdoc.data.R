7916606c571381136193ce465ec4b00fbd7d3d97
superuser mint18x64 <superuser-mint18x64@reyesaguayo.com>
2017-01-08 21:12:35 -0600

    dataset definition

---
+++ R/lsiex.termdoc.data.R
@@ -0,0 +1,16 @@
+#' Document terms from lsiex data set
+#'
+#' Used in Example 2-3 for Singular Value Decomposition
+#'
+#' @name lsiex.termdoc
+#'
+#' @docType data
+#'
+#'
+#' @format A data frame of 6x5
+#' @keywords datasets
+#'
+#' @examples
+#' lsiex.termdoc[1]
+NULL
+
