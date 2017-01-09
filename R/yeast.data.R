bd98e035b31057bd41969610db1b9276a636ffa1
superuser mint18x64 <superuser-mint18x64@reyesaguayo.com>
2017-01-08 21:12:50 -0600

    dataset def for yeast data

---
+++ R/yeast.data.R
@@ -0,0 +1,16 @@
+#' Random number for the normal distribution.
+#'
+#' Random numbers coming from Matlab example.
+#'
+#' @name yeast.data
+#'
+#' @docType data
+#'
+#'
+#' @format A data frame of 384x17
+#' @keywords datasets
+#'
+#' @examples
+#' yeast.data[1]
+NULL
+
