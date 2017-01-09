95d41873e7d0576a08e46a547ba6fe37091d80e2
superuser mint18x64 <superuser-mint18x64@reyesaguayo.com>
2017-01-08 21:09:19 -0600

    plot functions for chapter 2

---
+++ R/ch02-plot.R
@@ -0,0 +1,19 @@
+#' Draw Projection Matrix 2D Line
+#'
+#' draw the projection line given the projection matrix of angle theta
+#' @param M projection matrix
+#' @param ... other parameters for \code{\link{abline}}
+#'
+#' @export
+#'
+
+projectionMatrixLine <- function(M, ...) {
+  # draw infinite line given two point coordinates or vectors
+  segmentInf(M[,1], M[,2], ...)
+}
+
+segmentInf <- function(xs, ys, ...){
+  fit <- lm(ys~xs)
+  abline(fit, ...)
+}
+
