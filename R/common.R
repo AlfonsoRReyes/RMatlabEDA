26873afc5b156e244001edc80c5670921d83d70b
alfonso zbook <zbook@reyesaguayo.com>
2017-01-10 17:19:31 -0600

    new function m.rand. replicates Matlab's.

--- R/common.R
+++ R/common.R
@@ -5,6 +5,8 @@
 #' @param m number of rows
 #' @param n number of columns
 #'
+#' @importFrom stats runif
+#'
 #' @export
 #'
 #' @examples
@@ -14,5 +16,18 @@
 m.zeros <-function(m, n = 1) {
   # replicate the Matlab zeros function
   matrix(0, nrow = m, ncol = n)
+}
+
+
+#' Replicates Matlab's rand(m, n)
+#'
+#' Matrix of mxn uniform distribution numbers
+#'
+#' @param m number of rows
+#' @param n number of columns
+#'
+#' @export
+m.rand <- function(m = 1, n = 1) {
+  matrix(runif(m*n), n)
 }
 
